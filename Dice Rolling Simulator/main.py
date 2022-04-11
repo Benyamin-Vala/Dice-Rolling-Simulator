25f3a8fbb91dc488d5eb35e5e8a929103f5c94a1
Benyamin-Vala <102605148+Benyamin-Vala@users.noreply.github.com>
2022-04-11 16:38:06 +0300

    Add files via upload

---
+++ Dice Rolling Simulator/main.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+b = 1
+
+while(b == 1):
+    print('Please Enter For Get Random Dice.')
+    input()
+    d = randint(1,6)
+    print(f'The Random Dice number is : {d}')
+
+
+
+#------------------------- Coded By Benyamin Vala -----------------------------#
